552fb4d9487657ce02e16caf2b8e10a81b1ea823
hanulro1 <hanulro1@naver.com>
2020-12-21 18:06:24 +0900

    modified

--- app/src/main/java/com/example/team_project/ShockActivity.java
+++ app/src/main/java/com/example/team_project/ShockActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class ShockActivity extends AppCompatActivity {
+public class MainActivity9 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
